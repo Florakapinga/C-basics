f0afce7362f25a5a8a8ee45b89b3821044bf07b2
kapinga <erickfloriana@gmail.com>
2024-12-29 04:11:41 -0800

    the program that shows the declaration and assigning of integer data type

--- basics.cpp
+++ basics.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 int main()
 {
-    // this is a comment
-    /*
-    this is
-    mult line comment*/
-    std::cout << "hellow i like coding" << '\n';
-    std::cout << "its so funny" << '\n';
+    int x;  // declaration
+    x = 10; // assigning
+    std::cout << x;
 }
