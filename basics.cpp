39f45452878bb353cf83579cbea73422687d08e4
kapinga <erickfloriana@gmail.com>
2024-12-29 04:38:56 -0800

    double data type

--- basics.cpp
+++ basics.cpp
@@ -5,7 +5,11 @@ int main()
     int age = 20;
     int year = 2024;
     int day = 5.9; // trying to assign decimal number as a integer
-    std::cout << day;
+    // double (numbers including decimals)
+    double price = 900.9;
+    double gpa = 4.8;
+    double temperature = 32.7;
 
+    std::cout << gpa;
     return 0;
 }
