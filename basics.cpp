09b1213ddaf23687b96d63222925b4af79524913
kapinga <erickfloriana@gmail.com>
2024-12-29 04:25:57 -0800

    addition of two integer numbers

--- basics.cpp
+++ basics.cpp
@@ -3,7 +3,10 @@ int main()
 {
     int x = 10; // declaration and assigning
     int y = 100;
+    int sum = x + y;
     std::cout << x << std::endl;
     std::cout << y << std::endl;
+    std::cout << sum << std::endl;
+
     return 0;
 }
